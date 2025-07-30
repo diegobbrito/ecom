38ffde0bc79567aad51b1ce7287f0bfda3c98369
Diego Brito <dibbrito@gmail.com>
2025-07-30 20:02:38 -0300

    feat: Implement API server with routing for user management

---
+++ cmd/api/api.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/diegobbrito/ecom/service/user"
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	addr string
+	db   *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db:   db,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+	userStore := user.NewStore(s.db)
+	userHandler := user.NewHandler(userStore)
+	userHandler.RegisterRoutes(subrouter)
+
+	return http.ListenAndServe(s.addr, router)
+}
