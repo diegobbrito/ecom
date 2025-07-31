3f352c7f0957ef2f09dc5758810ea54b27de7dc1
Diego Brito <dibbrito@gmail.com>
2025-07-31 19:29:26 -0300

    feat: Implement user store with methods for retrieving users by email and ID

---
+++ service/user/store.go
@@ -0,0 +1,52 @@
+package user
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/diegobbrito/ecom/types"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{db: db}
+}
+
+func (s *Store) GetUserByEmail(email string) (*types.User, error) {
+	rows, err := s.db.Query("SELECT * FROM users WHERE email = ?", email)
+	if err != nil {
+		return nil, err
+	}
+
+	u := new(types.User)
+
+	for rows.Next() {
+		u, err = scanRowIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if u.ID == 0 {
+		return nil, fmt.Errorf("user not found")
+	}
+	return u, nil
+}
+
+func (s *Store) GetUserByID(id int) (*types.User, error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+func (s *Store) CreateUser(user types.User) error {
+	return fmt.Errorf("not implemented")
+}
+
+func scanRowIntoUser(rows *sql.Rows) (*types.User, error) {
+	user := new(types.User)
+	if err := rows.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.Password, &user.CreatedAt); err != nil {
+		return nil, err
+	}
+	return user, nil
+}
