6674ccf034bf0de6ae28d6cda91f1d27523fb11c
Diego Brito <dibbrito@gmail.com>
2025-08-14 20:16:35 -0300

    feat: Add store for cart

---
+++ service/order/store.go
@@ -0,0 +1,30 @@
+package cart
+
+import (
+	"database/sql"
+
+	"github.com/diegobbrito/ecom/types"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func (s *Store) CreateOrder(order types.Order) (int, error) {
+	res, err := s.db.Exec("INSERT INTO orders (user_id, total, status, address) VALUES (?, ?, ?, ?)",
+		order.UserID, order.Total, order.Status, order.Address)
+	if err != nil {
+		return 0, err
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return int(id), nil
+}
+
+func (s *Store) CreateOrderItem(orderItem types.OrderItem) error {
+	_, err := s.db.Exec("INSERT INTO order_items (order_id, product_id, quantity, price) VALUES (?, ?, ?, ?)",
+		orderItem.OrderID, orderItem.ProductID, orderItem.Quantity, orderItem.Price)
+	return err
+}
