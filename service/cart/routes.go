e60b340861d12d301dcadfd56ee9ef62b168075d
Diego Brito <dibbrito@gmail.com>
2025-08-16 20:33:13 -0300

    feat: Add routes for cart

---
+++ service/cart/routes.go
@@ -0,0 +1,62 @@
+package cart
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/diegobbrito/ecom/types"
+	"github.com/diegobbrito/ecom/utils"
+	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
+)
+
+type Handler struct {
+	store        types.OrderStore
+	productStore types.ProductStore
+}
+
+func NewHandler(store types.OrderStore, productStore types.ProductStore) *Handler {
+	return &Handler{store: store, productStore: productStore}
+}
+
+func (h *Handler) RegisterRoutes(router *mux.Router) {
+	router.HandleFunc("/cart/checkout", h.handleCheckout).Methods(http.MethodPost)
+}
+
+func (h *Handler) handleCheckout(w http.ResponseWriter, r *http.Request) {
+	var cart types.CartCheckoutPayload
+	if err := utils.ParseJSON(r, &cart); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err := utils.Validate.Struct(cart); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %v", errors))
+		return
+	}
+
+	productsIDs, err := getCartItemsIDs(cart.Items)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	ps, err := h.productStore.GetProductsByIDs(productsIDs)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+	userID := 0
+	orderID, totalPrice, err := h.createOrder(ps, cart.Items, userID)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, map[string]any{
+		"order_id": orderID,
+		"total":    totalPrice,
+	})
+
+}
