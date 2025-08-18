61c64948142834042bc2680633c4ffcf9fd87955
Diego Brito <dibbrito@gmail.com>
2025-08-18 20:49:30 -0300

    feat: Add jwt methods

--- service/auth/jwt.go
+++ service/auth/jwt.go
@@ -1,13 +1,23 @@
 package auth
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
 	"strconv"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 
 	"github.com/diegobbrito/ecom/config"
+	"github.com/diegobbrito/ecom/types"
+	"github.com/diegobbrito/ecom/utils"
 )
+
+type contextKey string
+
+const UserKey contextKey = "userID"
 
 func CreateJWT(secret []byte, userID int) (string, error) {
 	expiration := time.Second * time.Duration(config.Envs.JWTExpirationInSeconds)
@@ -21,4 +31,61 @@ func CreateJWT(secret []byte, userID int) (string, error) {
 		return "", err
 	}
 	return tokenString, nil
+}
+
+func WithJWTAuth(handlerFunc http.HandlerFunc, store types.UserStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tokenString := getTokenFromRequest(r)
+
+		token, err := validateToken(tokenString)
+		if err != nil {
+			log.Printf("failed to validate token: %v", err)
+			permissionDenied(w)
+			return
+		}
+
+		if !token.Valid {
+			log.Println("invalid token")
+			permissionDenied(w)
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+		str := claims["user_id"].(string)
+
+		userID, _ := strconv.Atoi(str)
+		u, err := store.GetUserByID(userID)
+		if err != nil {
+			log.Printf("failed to get user by ID %d: %v", userID, err)
+			permissionDenied(w)
+			return
+		}
+
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, UserKey, u.ID)
+		r = r.WithContext(ctx)
+		handlerFunc(w, r)
+
+	}
+}
+
+func getTokenFromRequest(r *http.Request) string {
+	tokenAuth := r.Header.Get("Authorization")
+	if tokenAuth != "" {
+		return tokenAuth
+	}
+	return ""
+}
+
+func validateToken(t string) (*jwt.Token, error) {
+	return jwt.Parse(t, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+		return []byte(config.Envs.JWTSecret), nil
+	})
+}
+
+func permissionDenied(w http.ResponseWriter) {
+	utils.WriteError(w, http.StatusForbidden, fmt.Errorf("permission denied"))
 }
